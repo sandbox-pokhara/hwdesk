1c9dcb07226d644b563c57e3b69232499684459b
Pradish Bijukchhe <pradishbijukchhe@gmail.com>
2024-05-30 00:16:20 +0545

    feat: add controls module

---
+++ hwdesk/controls/ch9329.py
@@ -0,0 +1,101 @@
+import time
+
+from ch9329 import keyboard
+from ch9329 import mouse
+from serial import Serial
+
+MODIFIERS = {
+    0x0001: "shift",
+    0x0004: "ctrl",
+    0x0008: "alt_left",  # does not work
+    0x0080: "alt_right",  # does not work
+    0x20000: "alt",
+}
+
+KEY_MAP = {
+    "space": " ",
+    "period": ".",
+    "backspace": "backspace",
+    "enter": "\r",
+    "escape": "\x1b",
+    "quotedbl": '"',
+    "quote": "'",
+    "grave": "`",
+    "minus": "-",
+    "equal": "=",
+    "asciitilde": "~",
+    "exclam": "!",
+    "at": "@",
+    "numbersign": "#",
+    "dollar": "$",
+    "percent": "%",
+    "asciicircum": "^",
+    "ampersand": "&",
+    "asterisk": "*",
+    "parenleft": "(",
+    "parenright": ")",
+    "underscore": "_",
+    "plus": "+",
+    "tab": "\t",
+    "caps_lock": "caps_lock",
+    "bracketleft": "[",
+    "bracketright": "]",
+    "braceleft": "{",
+    "braceright": "}",
+    "semicolon": ";",
+    "colon": ":",
+    "apostrophe": "'",
+    "backslash": "\\",
+    "bar": "|",
+    "comma": ",",
+    "less": "<",
+    "greater": ">",
+    "slash": "/",
+    "question": "?",
+    "return": "\n",
+    "prior": "page_up",
+    "next": "page_down",
+}
+
+
+class CH9329:
+    def __init__(self, port: str) -> None:
+        self.port = port
+        self.serial = Serial(port, 9600, timeout=0.05)
+        self.last_moved = time.time()
+        super().__init__()
+
+    def move(self, x: int, y: int) -> None:
+        # avoid spamming move
+        if time.time() - self.last_moved < 0.05:
+            return
+        # ignor negative values
+        if x < 0 or y < 0:
+            return
+        mouse.move(self.serial, x, y)
+        self.last_moved = time.time()
+
+    def click(self, button: str = "left") -> None:
+        mouse.press(self.serial, button)
+        mouse.release(self.serial)
+
+    def wheel(self, delta: int = 1) -> None:
+        mouse.wheel(self.serial, delta)
+
+    def press(self, key: str, state: int) -> None:
+        """
+        :param key: keysym attribute of tkinter key event
+        :param state: state attribute of tkinter key event
+        """
+        modifiers = [v for k, v in MODIFIERS.items() if k & state]
+        modifier = modifiers[0] if modifiers else ""
+        key = key.lower()
+        key = KEY_MAP.get(key, key)
+        keyboard.press(self.serial, key, modifier)
+
+    def release(self) -> None:
+        keyboard.release(self.serial)
+
+    def press_and_release(self, key: str, modifier: str = "") -> None:
+        keyboard.press(self.serial, key, modifier)
+        keyboard.release(self.serial)
---
+++ hwdesk/controls/prompt.py
@@ -0,0 +1,30 @@
+from tkinter.messagebox import showerror  # type:ignore
+from tkinter.simpledialog import askstring
+
+import serial.serialutil
+import serial.tools.list_ports
+from ch9329.config import get_serial_number
+from serial import Serial
+
+
+def ask_ch9329_port():
+    prompt = ""
+    prompt += "Please enter the index of CH9329 you want to access: \n"
+    valid_ports: list[str] = []
+    for i in serial.tools.list_ports.comports():
+        try:
+            if i.vid == 6790 and i.pid == 29987:
+                ser = Serial(i.name, 9600, timeout=0.05)
+                serial_number = get_serial_number(ser)
+                prompt += f"Port={i.name}, Serial={serial_number}"
+                valid_ports.append(i.name)
+        except serial.serialutil.SerialException:
+            pass
+    if valid_ports == []:
+        showerror("HwDesk Error", "No CH9329 devices found.")
+        return ""
+    port = askstring("CH9329 Selection", prompt, initialvalue=valid_ports[0])
+    if port not in valid_ports:
+        showerror("HwDesk Error", "Please select a valid CH9329 port.")
+        return ""
+    return port
