c6e00acdef1b68a1c2e2984e2e73b6e7691f5cdc
Pradish Bijukchhe <pradishbijukchhe@gmail.com>
2024-05-30 00:31:51 +0545

    fix: fix prompt message format

--- hwdesk/camera/prompt.py
+++ hwdesk/camera/prompt.py
@@ -5,17 +5,19 @@ from opencv_cam_idx.finder import find_cameras
 
 
 def ask_camera_idx() -> tuple[int, str]:
-    prompt = ""
-    prompt += "Please enter the index of camera you want to access: \n"
+    prompt: list[str] = []
+    prompt.append("Please enter the index of camera you want to access:")
     valid_idx: dict[int, tuple[int, str]] = {}
     for c in find_cameras():
         if "pid_2130" in c.device_path:
             valid_idx[c.idx] = c.idx, c.friendly_name
-            prompt += f"Index={c.idx}, Name={c.friendly_name}"
+            prompt.append(f"Index={c.idx}, Name={c.friendly_name}")
     if valid_idx == {}:
         showerror("HwDesk Error", "No MS2130 devices found.")
         return -1, ""
-    idx = askinteger("Camera Selection", prompt, initialvalue=0, minvalue=0)
+    idx = askinteger(
+        "Camera Selection", "\n".join(prompt), initialvalue=0, minvalue=0
+    )
     if idx not in valid_idx:
         showerror("HwDesk Error", "Please select a valid camera index.")
         return -1, ""
--- hwdesk/controls/prompt.py
+++ hwdesk/controls/prompt.py
@@ -8,22 +8,24 @@ from serial import Serial
 
 
 def ask_ch9329_port():
-    prompt = ""
-    prompt += "Please enter the index of CH9329 you want to access: \n"
+    prompt: list[str] = []
+    prompt.append("Please enter the index of CH9329 you want to access:")
     valid_ports: list[str] = []
     for i in serial.tools.list_ports.comports():
         try:
             if i.vid == 6790 and i.pid == 29987:
                 ser = Serial(i.name, 9600, timeout=0.05)
                 serial_number = get_serial_number(ser)
-                prompt += f"Port={i.name}, Serial={serial_number}"
+                prompt.append(f"Port={i.name}, Serial={serial_number}")
                 valid_ports.append(i.name)
         except serial.serialutil.SerialException:
             pass
     if valid_ports == []:
         showerror("HwDesk Error", "No CH9329 devices found.")
         return ""
-    port = askstring("CH9329 Selection", prompt, initialvalue=valid_ports[0])
+    port = askstring(
+        "CH9329 Selection", "\n".join(prompt), initialvalue=valid_ports[0]
+    )
     if port not in valid_ports:
         showerror("HwDesk Error", "Please select a valid CH9329 port.")
         return ""
