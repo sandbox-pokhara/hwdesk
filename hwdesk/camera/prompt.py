adea3a1a387bcd6f5bc40350db6139ee66980bd7
Pradish Bijukchhe <pradishbijukchhe@gmail.com>
2024-05-30 00:12:55 +0545

    refactor: improve camera prompt

--- hwdesk/camera/prompt.py
+++ hwdesk/camera/prompt.py
@@ -4,16 +4,19 @@ from tkinter.simpledialog import askinteger
 from opencv_cam_idx.finder import find_cameras
 
 
-def ask_camera_idx():
+def ask_camera_idx() -> tuple[int, str]:
     prompt = ""
-    prompt += "Please enter the index of server you want access: \n"
-    valid_idx: list[int] = []
+    prompt += "Please enter the index of camera you want to access: \n"
+    valid_idx: dict[int, tuple[int, str]] = {}
     for c in find_cameras():
         if "pid_2130" in c.device_path:
-            valid_idx.append(c.idx)
+            valid_idx[c.idx] = c.idx, c.friendly_name
             prompt += f"Index={c.idx}, Name={c.friendly_name}"
-    idx = askinteger("Server Selection", prompt, initialvalue=0, minvalue=0)
+    if valid_idx == {}:
+        showerror("HwDesk Error", "No MS2130 devices found.")
+        return -1, ""
+    idx = askinteger("Camera Selection", prompt, initialvalue=0, minvalue=0)
     if idx not in valid_idx:
-        showerror("HwDesk Error", "Please select a valid server index.")
-        return -1
-    return idx
+        showerror("HwDesk Error", "Please select a valid camera index.")
+        return -1, ""
+    return valid_idx[idx]
